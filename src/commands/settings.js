1fed21fbcf0898de9540d687e00a5a8632159223
Dennis Ploeger <develop@dieploegers.de>
2019-02-28 17:58:35 +0100

    Code optimizations

--- src/commands/settings.js
+++ src/commands/settings.js
@@ -2,6 +2,10 @@ var UI = require("sketch/ui");
 import BrowserWindow from "sketch-module-web-view";
 var Settings = require("sketch/settings");
 
+/**
+ * Use BrowserWindow to show the settings UI
+ */
+
 export default function() {
   const options = {
     identifier: "units.settingsView",
@@ -12,15 +16,30 @@ export default function() {
   const browserWindow = new BrowserWindow(options);
 
   browserWindow.loadURL(require("../../ui/settings-view.html"));
+
+  /**
+   * Close window on UI clicks
+   */
   browserWindow.webContents.on("closeSettings", function(s) {
     browserWindow.close();
   });
+
+  /**
+   * Save settings
+   */
   browserWindow.webContents.on("saveSettings", function(items) {
     Settings.setSettingForKey("units", items);
     browserWindow.close();
   });
+
+  /**
+   * Hide the error message on startup
+   */
   browserWindow.webContents.executeJavaScript("hideErrorMessage()");
 
+  /**
+   * Load the existing units
+   */
   if (Settings.settingForKey("units")) {
     Settings.settingForKey("units").forEach(function(unit) {
       browserWindow.webContents.executeJavaScript(

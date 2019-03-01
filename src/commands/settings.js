ba12b9be3353bfea5e1108ae8cda3df6e8ad0fc6
Dennis Ploeger <develop@dieploegers.de>
2019-03-01 13:42:40 +0100

    Moved js to separate file

--- src/commands/settings.js
+++ src/commands/settings.js
@@ -1,6 +1,6 @@
-var UI = require("sketch/ui");
-import BrowserWindow from "sketch-module-web-view";
-var Settings = require("sketch/settings");
+var UI = require('sketch/ui')
+import BrowserWindow from 'sketch-module-web-view'
+var Settings = require('sketch/settings')
 
 /**
  * Use BrowserWindow to show the settings UI
@@ -8,56 +8,52 @@ var Settings = require("sketch/settings");
 
 export default function() {
   const options = {
-    identifier: "units.settingsView",
+    identifier: 'units.settingsView',
     frame: false,
-    show: false
-  };
+    show: false,
+  }
 
-  const browserWindow = new BrowserWindow(options);
+  const browserWindow = new BrowserWindow(options)
 
-  browserWindow.loadURL(require("../../ui/settings-view.html"));
+  browserWindow.loadURL(require('../../ui/settings-view.html'))
 
   /**
    * Close window on UI clicks
    */
-  browserWindow.webContents.on("closeSettings", function(s) {
-    browserWindow.close();
-  });
+  browserWindow.webContents.on('closeSettings', function(s) {
+    browserWindow.close()
+  })
 
   /**
    * Save settings
    */
-  browserWindow.webContents.on("saveSettings", function(settings) {
-    Settings.setSettingForKey("units", settings.units);
-    Settings.setSettingForKey("defaultUnit", settings.defaultUnit);
-    browserWindow.close();
-  });
+  browserWindow.webContents.on('saveSettings', function(settings) {
+    Settings.setSettingForKey('units', settings.units)
+    Settings.setSettingForKey('defaultUnit', settings.defaultUnit)
+    browserWindow.close()
+  })
 
   /**
    * Hide the error message on startup
    */
-  browserWindow.webContents.executeJavaScript("hideErrorMessage()");
+  browserWindow.webContents.executeJavaScript('hideErrorMessage()')
 
   /**
    * Load the existing units
    */
-  if (Settings.settingForKey("units")) {
-    browserWindow.webContents.executeJavaScript(
-      `setUnits(${JSON.stringify(Settings.settingForKey("units"))})`
-    );
+  if (Settings.settingForKey('units')) {
+    browserWindow.webContents.executeJavaScript(`setUnits(${JSON.stringify(Settings.settingForKey('units'))})`)
   }
 
-  if (!Settings.settingForKey("defaultUnit")) {
-    Settings.setSettingForKey("defaultUnit", "_last");
+  if (!Settings.settingForKey('defaultUnit')) {
+    Settings.setSettingForKey('defaultUnit', '_last')
   }
 
-  browserWindow.webContents.executeJavaScript(
-    `setDefaultUnit('${Settings.settingForKey("defaultUnit")}')`
-  );
+  browserWindow.webContents.executeJavaScript(`setDefaultUnit('${Settings.settingForKey('defaultUnit')}')`)
 
-  browserWindow.webContents.executeJavaScript("redrawUI()");
+  browserWindow.webContents.executeJavaScript('redrawUI()')
 
-  browserWindow.once("ready-to-show", () => {
-    browserWindow.show();
-  });
+  browserWindow.once('ready-to-show', () => {
+    browserWindow.show()
+  })
 }
--- ui/js/settings-view.js
+++ ui/js/settings-view.js
@@ -1,183 +1,207 @@
 /* Bridges used by the plugin */
 
+let units = {}
+let defaultUnit = '_last'
+
 /**
- * Add a new unit to the units table
- * @param {Object} unit New unit object
- * @param {string} unit.unitName Name of unit (is used as an identifier)
- * @param {string} unit.dpi DPI value
- * @param {string} unit.factor Conversion factor
+ * Set the available units
  */
-window.addUnit = function(unit) {
-  const unitRow = $('<tr class="unitRow"></tr>');
-  unitRow.click(() => {
-    openEditUnitEditor(unitRow);
-  });
-  unitRow.css("cursor", "pointer");
+window.setUnits = function(pUnits) {
+  units = pUnits
+}
 
-  unitRow.data("unitName", unit.unitName);
-  unitRow.data("dpi", unit.dpi);
-  unitRow.data("factor", unit.factor);
+/**
+ * Set the default unit
+ */
+window.setDefaultUnit = function(pDefaultUnit) {
+  defaultUnit = pDefaultUnit
+}
 
-  unitRow.append("<td class=unitName>" + unit.unitName + "</td>");
-  unitRow.append("<td class=dpi>" + unit.dpi + "</td>");
-  unitRow.append("<td class=factor>" + unit.factor + "</td>");
+/**
+ * Redraw the UI
+ */
+window.redrawUI = () => {
+  $('#unitsTable .unitRow').remove()
+  $('#defaultUnitSelect option').remove()
 
-  $("#unitsTable tbody").append(unitRow);
-};
+  // Redraw the metric table
+
+  for (let unitName of Object.keys(units).sort()) {
+    const unit = units[unitName]
+    const unitRow = $('<tr class="unitRow"></tr>')
+    unitRow.click(() => {
+      openEditUnitEditor(unitRow)
+    })
+    unitRow.css('cursor', 'pointer')
+
+    unitRow.data('unitName', unit.unitName)
+    unitRow.data('dpi', unit.dpi)
+    unitRow.data('factor', unit.factor)
+
+    unitRow.append(`<td class="unitName">${unit.unitName}</td>`)
+    unitRow.append(`<td class="dpi">${unit.dpi}</td>`)
+    unitRow.append(`<td class="factor">${unit.factor}</td>`)
+
+    $('#unitsTable tbody').append(unitRow)
+  }
+
+  // Redraw the default metric selection
+  for (let unitName of ['_none', '_last'].concat(Object.keys(units))) {
+    let description = unitName
+
+    if (unitName === '_none') {
+      description = 'None'
+    } else if (unitName === '_last') {
+      description = 'Last selected'
+    }
+
+    let selected = ''
+
+    if (unitName === defaultUnit) {
+      selected = ' selected'
+    }
+
+    $('#defaultUnitSelect').append(`<option value="${unitName}"${selected}>${description}</option>`)
+  }
+}
 
 /**
  * Show an error message
  */
 window.showErrorMessage = function(message) {
-  $("#errorMessage").html(message);
-  $("#errorMessage").show();
-};
+  $('#errorMessage').html(message)
+  $('#errorMessage').show()
+}
 
 /**
  * Hide the error message alert
  */
 window.hideErrorMessage = function() {
-  $("#errorMessage").hide();
-};
+  $('#errorMessage').hide()
+}
 
 /**
  * UI handling
  */
 
 checkFormValidity = function() {
-  return $("input", "#unitEditorForm")
+  return $('input', '#unitEditorForm')
     .toArray()
     .reduce(function(isValid, item) {
-      if (item.id === "unitName") {
-        var exists = $(".unitRow")
-          .toArray()
-          .reduce(function(doesNameExist, existingItem) {
-            if ($(existingItem).data("unitName") === item.value) {
-              doesNameExist = true;
-            }
-            return doesNameExist;
-          }, false);
-        if (exists) {
-          isValid = false;
-          item.setCustomValidity("Name already exists");
+      if (item.id === 'unitName') {
+        if (units.hasOwnProperty(item.value)) {
+          isValid = false
+          item.setCustomValidity('Name already exists')
         }
       }
       if (!item.checkValidity()) {
-        isValid = false;
+        isValid = false
       }
-      return isValid;
-    }, true);
-};
+      return isValid
+    }, true)
+}
 
 /**
  * Open the unit editor to add a new unit
  */
 openNewUnitEditor = function() {
-  $("#editWindow").modal();
+  $('#editWindow').modal()
 
   // Remove event triggers on modal hide
-  $("#editWindow").on("hidden.bs.modal", function() {
-    $("#unitEditorOk").off();
-  });
+  $('#editWindow').on('hidden.bs.modal', function() {
+    $('#unitEditorOk').off()
+  })
 
   // reset input fields
 
-  $("#unitName")
-    .val("")
-    .focus();
-  $("#dpi").val("");
-  $("#factor").val("");
+  $('#unitName')
+    .val('')
+    .focus()
+  $('#dpi').val('')
+  $('#factor').val('')
 
   // Hide delete button for new units
-  $("#unitEditorDelete").hide();
+  $('#unitEditorDelete').hide()
 
-  $("#unitEditorOk").click(function() {
+  $('#unitEditorOk').click(function() {
     // Validate form
-    var isFormValid = checkFormValidity();
+    var isFormValid = checkFormValidity()
     if (isFormValid === false) {
       // Mimic a form submit to trigger form validation messages
       $('<input type="submit">')
         .hide()
-        .appendTo($("#unitEditorForm"))
+        .appendTo($('#unitEditorForm'))
         .click()
-        .remove();
+        .remove()
     } else {
       // Add the unit
-      window.addUnit({
-        unitName: $("#unitName").val(),
-        dpi: $("#dpi").val(),
-        factor: $("#factor").val()
-      });
-      $("#editWindow").modal("hide");
+      units[$('#unitName').val()] = {
+        unitName: $('#unitName').val(),
+        dpi: $('#dpi').val(),
+        factor: $('#factor').val(),
+      }
+      $('#editWindow').modal('hide')
+      redrawUI()
     }
-  });
-};
+  })
+}
 
 /**
  * Open the unit editor to modify an existing unit
  * @param {Object} unitRow the table row holding the existing unit
  */
 openEditUnitEditor = function(unitRow) {
-  $("#editWindow").modal();
+  const originalUnitName = unitRow.data('unitName')
+  $('#editWindow').modal()
 
   // Remove event triggers on modal hide
 
-  $("#editWindow").on("hidden.bs.modal", function() {
-    $("#unitEditorOk").off();
-    $("#unitEditorDelete").off();
-  });
+  $('#editWindow').on('hidden.bs.modal', function() {
+    $('#unitEditorOk').off()
+    $('#unitEditorDelete').off()
+  })
 
   // Set input field values
-  $("#unitName")
-    .val(unitRow.data("unitName"))
-    .focus();
-  $("#dpi").val(unitRow.data("dpi"));
-  $("#factor").val(unitRow.data("factor"));
+  $('#unitName')
+    .val(unitRow.data('unitName'))
+    .focus()
+  $('#dpi').val(unitRow.data('dpi'))
+  $('#factor').val(unitRow.data('factor'))
 
   // React to delete action
-  $("#unitEditorDelete")
+  $('#unitEditorDelete')
     .show()
     .click(function() {
-      $("#editWindow").modal("hide");
-      $(unitRow).remove();
-    });
-  $("#unitEditorOk").click(function() {
+      $('#editWindow').modal('hide')
+      $(unitRow).remove()
+    })
+  $('#unitEditorOk').click(function() {
     // Validate form
-    var isFormValid = checkFormValidity();
+    var isFormValid = checkFormValidity()
     if (isFormValid === false) {
       // Mimic a form submit to trigger form validation messages
       $('<input type="submit">')
         .hide()
-        .appendTo($("#unitEditorForm"))
+        .appendTo($('#unitEditorForm'))
         .click()
-        .remove();
+        .remove()
     } else {
       // Change row to match the edited fields
-      unitRow.data("unitName", $("#unitName").val());
-      unitRow.data("dpi", $("#dpi").val());
-      unitRow.data("factor", $("#factor").val());
-      $(".unitName", unitRow).text($("#unitName").val());
-      $(".dpi", unitRow).text($("#dpi").val());
-      $(".factor", unitRow).text($("#factor").val());
-      $("#editWindow").modal("hide");
+      delete units[originalUnitName]
+      units[$('#unitName').val()] = {
+        unitName: $('#unitName').val(),
+        dpi: $('#dpi').val(),
+        factor: $('#factor').val(),
+      }
+      redrawUI()
+      $('#editWindow').modal('hide')
     }
-  });
-};
+  })
+}
 
 /**
- * Gather all units from the unit table and call the plugin to store them
+ * Call the plugin to store the current settings
  */
 saveSettings = function() {
-  var units = [];
-  $(".unitRow")
-    .toArray()
-    .forEach(function(value) {
-      var item = $(value);
-      units.push({
-        unitName: item.data("unitName"),
-        dpi: item.data("dpi"),
-        factor: item.data("factor")
-      });
-    });
-  window.postMessage("saveSettings", units);
-};
+  window.postMessage('saveSettings', { units: units, defaultUnit: defaultUnit })
+}
